3cdbd144cb35fd1758bfa71820201570686ba155
Jonathan Wilsson <jonathan.wilsson@gmail.com>
2011-05-28 16:42:01 +0200

    version 1.0.3

---
+++ README.md
@@ -0,0 +1,35 @@
+# jQuery LightTip 1.0.3
+LightTip is a jQuery plugin which allows you to create simple little tooltips on any element.
+
+http://jonathanwilsson.com/projects/jquery-lighttip/
+
+## Features
+* Tiny, 534 bytes minified and gzipped.
+* Lock the position of the tooltip or let it follow the mouse.
+* Animated display of the toolip
+
+### Options
+* animate (Default true) true/false Whether to animate the display of the tooltip
+* lockPosition (Default false) true/false Whether to lock the position of the tooltip
+
+Requires at least jQuery 1.5.
+
+## License
+Free to use and abuse under the [MIT license](http://www.opensource.org/licenses/mit-license.php).
+
+## Changelog
+### 1.0.3
+* Added option 'animate' to allow for animated display of the tooltip
+
+### 1.0.2
+* Code optimization and cleanup
+* Added option 'lockPosition' to prevent the tooltip from following the mouse
+
+### 1.0.1
+* Rewrote bits of the code to perform better
+
+### 1.0
+* Initial release
+
+## Copyright
+Copyright 2011 Jonathan Wilsson
--- jquery.lighttip - Kopia.js
+++ jquery.lighttip - Kopia.js
@@ -1,5 +1,5 @@
 /**
- * jQuery LightTip 1.0.3
+ * jQuery LightTip 1.0.2
  * http://jonathanwilsson.com/projects/lighttip/
  *
  * Copyright 2011 Jonathan Wilsson
@@ -12,7 +12,6 @@
 
     $.fn.LightTip = function (options) {
 	var defaults = {
-	    animate: true,
 	    lockPosition: false  
 	};
 	
@@ -22,60 +21,49 @@
 	
         return this.each(function () {
             // Only create one tooltip div
-            if ($(".tooltip").length <= 0) {
-                $("body").append('<div class="tooltip"></div>');
+            if ($('.tooltip').length <= 0) {
+                $('body').append('<div class="tooltip"></div>');
             }
 
             var $this = $(this),
-		$tooltip = $(".tooltip");
+		$tooltip = $('.tooltip');
 
             $tooltip.hide();
 
             // Add the tooltip when the user hovers over the specified element
-            $this.bind("mouseover", function (e) {
-                var title = $this.attr("title");
+            $this.bind('mouseover', function (e) {
+                var title = $this.attr('title');
 
                 // Clear the title attribute temporarily
-                $(this).data("orgTitle", title).attr("title", "");
+                $(this).data('orgTitle', title).attr('title', '');
 
                 // Show the tooltip
                 $tooltip.html(title)
 			.css({
+			    display: 'block',
 			    left: e.pageX ,
-			    position: "absolute",
+			    position: 'absolute',
 			    top: e.pageY
 			});
-		
-		if (defaults.animate) {
-		    $tooltip.animate({opacity: "show", height: "show"});
-		} else {
-		    $tooltip.show();
-		}
 	    });
 	    
-            $this.bind("mouseout", function () {
+            $this.bind('mouseout', function () {
                 var $elem = $(this);
 		
                 // Restore the title attribute's value
-                $elem.attr("title", $elem.data("orgTitle"));
+                $elem.attr('title', $elem.data('orgTitle'));
 
                 // Hide the tooltip
-		if (defaults.animate) {
-		    $tooltip.animate({opacity: "hide", height: "hide"}, 400, function () {
-			$(this).html(""); // Called here to prevent the content from being emptied prematurely
-		    });
-		} else {
-		    $tooltip.hide().html("");
-		}		
+                $tooltip.html('').hide();
             });
 
             // Make the tooltip follow the mouse
 	    if (!defaults.lockPosition) {
-		$this.bind("mousemove", function (e) {
+		$this.bind('mousemove', function (e) {
 		    $tooltip.css({
-			left: e.pageX + 10,
-			position: "absolute",
-			top: e.pageY + 10
+			'left': e.pageX + 10,
+			'position': 'absolute',
+			'top': e.pageY + 10
 		    });
 		});
 	    }
